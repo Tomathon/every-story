4fe41ae7e64fde7b7279fb99a81bc87e4dbca47c
Tom Miller <tomathonmiller@gmail.com>
2019-01-17 20:15:29 +0000

    Create navigation template part

---
+++ wp-content/themes/every-story/template-parts/navigation.php
@@ -0,0 +1,9 @@
+<div class="grid-x align-center">
+	<div class="cell medium-8">
+		<nav class="site-navigation top-bar" role="navigation">
+			<div class="top-bar-right">
+				<?php foundationpress_top_bar_r(); ?>
+			</div>
+		</nav>
+	</div>
+</div>
