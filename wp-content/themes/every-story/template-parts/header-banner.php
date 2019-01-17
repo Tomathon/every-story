e0ac29a42f49619656c0d99d3f8cacc546673cef
Tom Miller <tomathonmiller@gmail.com>
2019-01-17 20:09:22 +0000

    Create header-banner template part

---
+++ wp-content/themes/every-story/template-parts/header-banner.php
@@ -0,0 +1,9 @@
+<header class="grid-x align-center">
+	<div class="cell medium-6 large-8 text-center">
+        <div class="header-banner">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                <h1><?php bloginfo( 'name' ); ?></h1>
+            </a>
+        </div>
+	</div>
+</header>
